64753e488a458f7bcce15143d7f051b033ac099e
gerrykastogi <gerrykastogi@gmail.com>
2017-06-19 11:22:50 +0700

    update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,16 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        // call uses table seeder class
-        $this->call('OrderLinesTableSeeder');
-        $this->call('OrdersTableSeeder');
-        $this->call('UsersTableSeeder');
-        //this message shown in your terminal after running db:seed command
-        // $this->command->info("Order Line table seeded");
-=======
+        // $this->call('OrderLinesTableSeeder');
+        // $this->call('OrdersTableSeeder');
+        // $this->call('UsersTableSeeder');
         $this->call(OrderStatusSeeder::class);
         $this->call(ReasonSeeder::class);
->>>>>>> 2a8b60c799c237cf60c8fda9c1e2ad481ee1c1a0
     }
 }
